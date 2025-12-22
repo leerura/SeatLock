e6600ca01936e437fe7dd197da4e8498e344656e
leerura <sw09112@naver.com>
2025-12-22 17:20:41 +0900

    fix: 코드 리뷰 반영: 미사용 import 구문 삭제

--- src/main/java/com/seatlock/seatlock/health/HealthCheckController.java
+++ src/main/java/com/seatlock/seatlock/health/HealthCheckController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/v1")
 public class HealthCheckController {

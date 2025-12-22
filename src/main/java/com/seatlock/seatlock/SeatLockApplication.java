5194dbf6a7ac7830f20f116604696248b3f9a104
leerura <sw09112@naver.com>
2025-12-22 15:43:40 +0900

    INIT

---
+++ src/main/java/com/seatlock/seatlock/SeatLockApplication.java
@@ -0,0 +1,13 @@
+package com.seatlock.seatlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seatlock/seatlock/SeatLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatlock.seatlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
